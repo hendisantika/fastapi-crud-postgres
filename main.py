57750e328b09439a215e9c33cf670ae71de845e6
hendisantika <hendisantika@yahoo.co.id>
2024-11-21 19:33:15 +0700

    Add create Product API🫸🌀✏️📗🐧🐳⬆

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from datetime import time
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy.dialects.postgresql import psycopg2
+from sqlalchemy.orm import Session
 
 import models
-from database import engine
+from database import engine, get_db
+from schemas import Products
 
 while True:
     try:
@@ -25,3 +27,12 @@ models.Base.metadata.create_all(bind=engine)
 @app.get("/")
 def posts():
     return {"message": "this is working"}
+
+
+@app.post("/product")
+def create(product: Products, db: Session = Depends(get_db)):
+    new_product = models.Product(**product.dict())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
