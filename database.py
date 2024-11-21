8d08819da02ea116bb0f75bea04b93f0297ac3dd
hendisantika <hendisantika@yahoo.co.id>
2024-11-21 19:28:47 +0700

    Add database.py n .gitignore🫸🌀✏️📗🐧🐳⬆

---
+++ database.py
@@ -0,0 +1,15 @@
+SQLALCHEMY_DATABASE_URL = "postgresql://yuji:53cret@localhost/ecommerce"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
