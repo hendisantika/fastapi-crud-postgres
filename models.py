bdc1d4f9012937a95f4f1e2ed3b76c9f7fddd9a7
hendisantika <hendisantika@yahoo.co.id>
2024-11-21 19:29:49 +0700

    Add Product🫸🌀✏️📗🐧🐳⬆

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, Boolean, TIMESTAMP, text, String
+
+from database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    at_sale = Column(Boolean, server_default=text('false'))
+    inventory = Column(Integer, server_default=text('0'), nullable=False)
+    added_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('Now()'))
